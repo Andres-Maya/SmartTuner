868da6eb7d734a5a89780e0367d108efbae158d0
Nayak <andrescamilomaya07@gmail.com>
2026-09-14 18:36:17 -0500

    Modify The Main Activity of te program

--- app/src/main/java/com/andres/smarttuner/MainActivity.kt
+++ app/src/main/java/com/andres/smarttuner/MainActivity.kt
@@ -1,47 +1,30 @@
 package com.andres.smarttuner
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.andres.smarttuner.tuner.TunerViewModel
 import com.andres.smarttuner.ui.theme.SmartTunerTheme
+import com.andres.smarttuner.ui.tuner.TunerRoute
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: TunerViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+        )
         setContent {
             SmartTunerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TunerRoute(viewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartTunerTheme {
-        Greeting("Android")
     }
 }
